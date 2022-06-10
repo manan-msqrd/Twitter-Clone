c22aac5f16f6adad3a1cbc4c472ae46d8a1567bd
Manan <mananmanchanda@gmail.com>
2022-06-10 22:21:36 +0530

    step-2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter_clone/constants/routes.dart';
 import 'package:flutter_twitter_clone/homepage.dart';
+import 'package:flutter_twitter_clone/services/auth/auth_exceptions.dart';
+import 'package:flutter_twitter_clone/services/auth/auth_service.dart';
+import 'package:flutter_twitter_clone/utilities/show_error_dialogbox';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +12,23 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late String _email;
-  late String _password;
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    super.initState();
+    _email = TextEditingController();
+    _password = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,24 +55,25 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 20,
             ),
             TextField(
+              controller: _email,
+              autocorrect: false,
+              enableSuggestions: false,
+              keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
                 hintText: 'Enter Your email',
               ),
-              onChanged: (value) {
-                _email = value;
-              },
             ),
             const SizedBox(
               height: 30,
             ),
             TextField(
+              controller: _password,
+              autocorrect: false,
+              enableSuggestions: false,
               obscureText: true,
               decoration: const InputDecoration(
                 hintText: 'Enter Your password',
               ),
-              onChanged: (value) {
-                _password = value;
-              },
             ),
             const SizedBox(
               height: 180,
@@ -70,8 +90,43 @@ class _LoginScreenState extends State<LoginScreen> {
                   const Size(300, 60),
                 ),
               ),
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const HomePage())),
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  await AuthService.firebase()
+                      .logIn(email: email, password: password);
+                  final user = AuthService.firebase().currentUser;
+                  if (user?.isEmailVerified ?? false) {
+                    //user is verified
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (route) => false,
+                    );
+                  } else {
+                    //user is NOT verified
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (route) => false,
+                    );
+                  }
+                } on UserNotFoundAuthException {
+                  await showErrorDialog(
+                    context,
+                    'User Not Found',
+                  );
+                } on WrongPasswordAuthException {
+                  await showErrorDialog(
+                    context,
+                    'Wrong Password',
+                  );
+                } on GenericAuthException {
+                  await showErrorDialog(
+                    context,
+                    'Authentication Error',
+                  );
+                }
+              },
               child: const Text(
                 'LOG IN',
                 style: TextStyle(
